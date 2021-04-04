0798b2aa934949439616c9e13411ae7aa1100430
Jeff Robbins <jeffr@livedata.com>
2021-04-04 13:29:43 -0400

    lambda code edit 3 (no self_mutating)

--- pipeline_test4/lambda/handler.py
+++ pipeline_test4/lambda/handler.py
@@ -3,6 +3,6 @@ from datetime import datetime
 
 def handler(event, context):
     return {
-        'body': f'Hello from Lambda pipeline-test4 edit 2! The time is {datetime.now().isoformat()}',
+        'body': f'Hello from Lambda pipeline-test4 edit 3! The time is {datetime.now().isoformat()}',
         'statusCode': '200'
     }
