f87ab6d7568f01146712afcfd38adc4075710c7c
Jeff Robbins <jeffr@livedata.com>
2021-04-04 14:10:05 -0400

    lambda code edit 5

--- pipeline_test4/lambda/handler.py
+++ pipeline_test4/lambda/handler.py
@@ -3,6 +3,6 @@ from datetime import datetime
 
 def handler(event, context):
     return {
-        'body': f'Hello from Lambda pipeline-test4 edit 4! The time is {datetime.now().isoformat()}',
+        'body': f'Hello from Lambda pipeline-test4 edit 5! The time is {datetime.now().isoformat()}',
         'statusCode': '200'
     }
